e9d976ccb6a46eb7ee1258daa5e36eb2b2d11bae
Sylvester Damgaard <sn@cbox.dk>
2026-04-19 16:20:05 +0200

    release: damgaard_dashboards v0.3.5

---
+++ damgaard_dashboards/rootfs/app/_app/immutable/entry/start.DwyJCGI2.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BbCD9ffl.js";export{o as load_css,r as start};
--- damgaard_dashboards/rootfs/app/_app/immutable/entry/start.tD0Rxtt2.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CCpCeT2c.js";export{o as load_css,r as start};
